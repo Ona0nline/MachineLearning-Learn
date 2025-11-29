f1988b2eb25b66cd668d695bf9e89c1ef199cdc1
Ona0nline <onasihle123@gmail.com>
2025-11-29 08:03:45 +0200

    Defined imports and reasons for importing, creating data frame curr

---
+++ main.py
@@ -0,0 +1,23 @@
+# Goal is to create a model using the data to predict my surviviabilty on the titanic based off of the data point dropped in
+# DATA
+import pandas as pd
+# Load and creating data frame
+import numpy as np
+
+# MACHINE LEARNING
+# Takes list of data and converts it into an array, model understandds data better
+from sklearn.model_selection import train_test_split, GridSearchCV
+# Allows us to create training set and testing set, test multiple scenarios
+from sklearn.preprocessing import MinMaxScaler
+# Cleans data
+from sklearn.neighbors import KNeighborsClassifier
+# Model we will be using
+from sklearn.metrics import accuracy_score,confusion_matrix
+# Metrics to test how our model is performing
+
+# VISUALISATION
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Creating a data frame
+data = pd.read_csv("titanic.csv")
